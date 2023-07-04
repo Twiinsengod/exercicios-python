322b5f3c9a6b8bebe7e5a91c74af6235f72a35fb
Twiinsengod <crufabio@gmail.com>
2023-07-04 19:33:56 -0300

    exercicio7.py

---
+++ exercicio7.py
@@ -0,0 +1,8 @@
+## Desenvolva um programa que leia as duas notas de um aluno. calcule e mostre a sua média.
+
+num1 = int(input("informe o 1º valor: "))
+num2 = int(input("informe o 2º valor: "))
+
+media = (num1 + num2) / 2
+
+print("A media dos valores é: ", media)
