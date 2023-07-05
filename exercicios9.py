8d50bf2395df5e300f9c97257cd233d79a97b0d5
Twiinsengod <crufabio@gmail.com>
2023-07-05 03:21:08 -0300

    exercicios9.py

---
+++ exercicios9.py
@@ -0,0 +1,14 @@
+## Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+
+numero = int(input("Insira um número inteiro qualquer: "))
+
+print('|', numero, '* 1 = ', numero*1)
+print('|', numero, '* 2 = ', numero*2)
+print('|', numero, '* 3 = ', numero*3)
+print('|', numero, '* 4 = ', numero*4)
+print('|', numero, '* 5 = ', numero*5)
+print('|', numero, '* 6 = ', numero*6)
+print('|', numero, '* 7 = ', numero*7)
+print('|', numero, '* 8 = ', numero*8)
+print('|', numero, '* 9 = ', numero*9)
+print('|', numero, '* 10 = ', numero*10)
