45deee6efbcdbe1f47f8a1f7d4877b3d7954c2ad
Twiinsengod <crufabio@gmail.com>
2023-07-05 04:31:08 -0300

    desafio03.py

---
+++ desafio03.py
@@ -0,0 +1,8 @@
+## Crie um script Python que leia dois numeros e tente mostrar a soma entre eles.
+
+numero1 = int(input("Digite o 1º valor: "))
+numero2 = int(input("Digite o 2º valor: "))
+
+soma = numero1+numero2
+
+print("A soma dos valores é:",soma)
