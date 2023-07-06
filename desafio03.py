556bd5cf2e4fce81f98217f27b7dd28dd5b90b57
Twiinsengod <crufabio@gmail.com>
2023-07-06 14:57:08 -0300

    desafio04.py

--- desafio03.py
+++ desafio03.py
@@ -2,7 +2,6 @@
 
 numero1 = int(input("Digite o 1º valor: "))
 numero2 = int(input("Digite o 2º valor: "))
-
 soma = numero1+numero2
 
-print("A soma dos valores é:",soma)
+print("A soma entre os valores {} e {} vale {}!".format(numero1, numero2 ,soma))
--- exercicio8.py
+++ exercicio8.py
@@ -4,7 +4,10 @@ metros = int(input("Informe o valor em metros: "))
 print("metro(s):", metros)
 
 centimetros = metros * 100
-print("O valor em centímetros é: {:.2f}".format(centimetros).replace('.', ','))
-
+print("O valor em centímetros é: {:.2f}".format(centimetros))
 milimetros = metros * 1000
-print("O valor em milímetros é: {:,}".format(milimetros).replace(',', '.'))
+print("O valor em milímetros é: {:,}".format(milimetros))
+
+valor_extenso = "{:,.0f}".format(metros)
+print("O valor em metros é:", valor_extenso)
+
