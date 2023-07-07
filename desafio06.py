d94afe011b80269478ddf463bf1769df607e00ea
Twiinsengod <crufabio@gmail.com>
2023-07-06 23:02:40 -0300

    desafio06.py

---
+++ desafio06.py
@@ -0,0 +1,6 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada.
+
+n = int(input('Digite um valor: '))
+print('O dobro do valor {} é {}'.format(n, (n*2)))
+print('O triplo do valor {} é {}'.format(n, (n*3)))
+print('A raiz quadrada do valor {} é {:.2f}'.format(n, (n**0.5)))
