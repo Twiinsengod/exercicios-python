7df15ee73168d4b07e81e0683d7393ea7bf8ba5c
Twiinsengod <crufabio@gmail.com>
2023-07-06 22:19:59 -0300

    exeercicio04.py

--- exercicio04.py
+++ exercicio04.py
@@ -1,8 +1,8 @@
-## Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
 a = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(a))
-print('Só tem espaços? ',a.isspace())
+print('Só tem espaços?',a.isspace())
 print('É um número?',a.isnumeric())
 print('É alfabético?',a.isalpha())
 print('É alfanumérico?',a.isalnum())
