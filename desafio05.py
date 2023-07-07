f158f1745151ae629f390534010d6dfb4bcb0030
Twiinsengod <crufabio@gmail.com>
2023-07-06 22:43:58 -0300

    desafio05.py

---
+++ desafio05.py
@@ -0,0 +1,4 @@
+## Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número qualquer: '))
+print('Analisando o valor {}, O seu antecessor é {} e o seu sucessor é {}'.format(n, (n-1), (n+1)))
