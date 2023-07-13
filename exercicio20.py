1609268b444be2b474a324edabba79a286dc2b15
Twiinsengod <crufabio@gmail.com>
2023-07-13 08:42:16 -0300

    exercicio10.py

--- exercicio20.py
+++ exercicio20.py
@@ -1,12 +1,12 @@
 # Faça um programa que sorteia a ordem de apresentação de trabalhos dos alunos. Façum programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
-import random
+from random import shuffle
 
 n1 = str(input('Primeiro(a) aluno(a): '))
 n2 = str(input('Segundo(a) aluno(a): '))
 n3 = str(input('Terceiro(a) aluno(a): '))
 n4 = str(input('Quarto(a) aluno(a): '))
 lista = [n1,n2,n3,n4]
-random.shuffle(lista)
+shuffle(lista)
 print('A ordem de apresentação será ')
 print(lista)
